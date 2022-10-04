b05330c57a39102c59d1b95ef1aefe712c18289b
byun sung yul <qustjdduf@gmail.com>
2022-10-04 18:31:55 +0900

    first commit

---
+++ src/main/kotlin/level1456/main.kt
@@ -0,0 +1,19 @@
+package level1456
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+class main
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+
+    val A = st.nextToken().toLong()
+    val B = st.nextToken().toLong()
+    val size1 = 10000000
+
+
+
+}
